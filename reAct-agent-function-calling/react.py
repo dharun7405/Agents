45fe6ab351b4d69176f14d06fd1e29a2b1482781
dharun7405 <dharunsudha74@gmail.com>
2026-05-06 08:29:13 +0530

    function calling added

---
+++ reAct-agent-function-calling/react.py
@@ -0,0 +1,17 @@
+from dotenv import load_dotenv
+from langchain_core.tools import tool
+from langchain_tavily import TavilySearch
+from langchain_groq import ChatGroq
+
+load_dotenv()
+
+@tool
+def triple(num:float) -> float:
+    """
+    param num: a number to triple
+    returns: the triple of the input number
+    """
+    return float(num) * 3
+
+tools = [TavilySearch(max_results=1), triple]
+llm = ChatGroq(model="Llama-3-Groq-8B-Tool-Use",temperature=0).bind_tools(tools)
